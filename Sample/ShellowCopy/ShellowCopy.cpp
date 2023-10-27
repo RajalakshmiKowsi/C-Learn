a87c579fbe9e89117ac754ee17b33e29504726ed
Rajalakshmi <rajalakshmisai26@gmail.com>
2023-10-27 10:47:01 +0530

    Interface and Polymorphsim.cpp

--- Sample/ShellowCopy/ShellowCopy.cpp
+++ Sample/ShellowCopy/ShellowCopy.cpp
@@ -14,8 +14,14 @@ public:
 	int get_data() { return *data; }
 	Shallow(int d);
 	Shallow(const Shallow& sourse);
+	
+	void display_shallow(Shallow s)
+	{
+		cout << get_data() << endl;
+	}
 	~Shallow();
 };
+
 Shallow::Shallow(int d)
 {
 	data = new int;
@@ -32,6 +38,9 @@ Shallow::~Shallow()
 }
 int main()
 {
-
+	Shallow obj1(100);
+	obj1.display_shallow(obj1);
+	Shallow obj2(obj1);
+	obj1.display_shallow(1000);
 }
 
